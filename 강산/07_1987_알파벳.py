d627ea67353ec0857e917b4b62742c285eb35467
kangsan <piopiop1178@gmail.com>
2020-12-25 19:01:33 +0900

    .

---
+++ 강산/07_1987_알파벳.py
@@ -0,0 +1,27 @@
+import sys
+sys.stdin = open("input.txt", "r")  
+
+R, C = map(int, sys.stdin.readline().split())
+board = [list(map(lambda x:ord(x) - 65, sys.stdin.readline().rstrip())) for _ in range(R)]
+check = [[0] * C for _ in range(R)]
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+check = [False] * 26
+answer = 1
+
+def dfs(x, y, n):
+    global answer
+    n += 1
+    answer = max(answer, n)
+    
+    check[board[y][x]] = True
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if 0 <= nx < C and 0 <= ny < R:
+            if not check[board[ny][nx]] :
+                dfs(nx, ny, n)
+    check[board[y][x]] = False
+
+dfs(0, 0, 0)
+print(answer)
+#왜 set이 더 느린지???? 
